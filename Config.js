38719ead608862a87328ffd67e1e9f082f03c8ac
madariss5 <menyhartmartin4@gmail.com>
2025-02-18 22:09:54 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['254745936840']
-global.ownername = "KISH"//owner name
+global.ownernumber = ['4915563151347']
+global.ownername = "Martin"//owner name
 global.ytname = "YT: Kish-MD"
 global.socialm = "GitHub: Kish-MD"
 global.location = "Kenya"
